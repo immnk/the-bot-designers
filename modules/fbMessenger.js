415b241a8eb9f849f03b6eea6366e7b31a96ff3e
Mani <im.manikanta@gmail.com>
2017-05-04 12:27:47 +0530

    new fix

--- modules/fbMessenger.js
+++ modules/fbMessenger.js
@@ -634,8 +634,8 @@ function sendAccountLinking(recipientId) {
  */
 function callSendAPI(messageData) {
     request({
-        uri: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: PAGE_ACCESS_TOKEN },
+        uri: constants.FB_MESSAGES_URL,
+        qs: { access_token: constants.PAGE_ACCESS_TOKEN },
         method: 'POST',
         json: messageData
 
