5f638d117257fbdabd8675326400170e3d53f375
Mani <im.manikanta@gmail.com>
2017-05-06 12:10:36 +0530

    fb end point creation

--- modules/constants.js
+++ modules/constants.js
@@ -1,6 +1,10 @@
 module.exports = {
     "PAGE_ACCESS_TOKEN": "EAACw1l6qxqsBAJsKETz7nwSRMesdxqi9YF1CZAfxYbUkTe5V5fp7Ada2dER4gBhNGY8mHS2ltOcTsYUwrGef5LGjipKjp8BdZCDYrZAQHZCYVNUJlezm8P9gBMPEmWI28lDvDWVKYTC1FekZCEIhkvxjaqhYkz0Y9jZB1xdhgxZAwZDZD",
     "FB_MESSAGES_URL": "https://graph.facebook.com/v2.6/me/messages",
+    "FD": {
+        "FD_API_KEY": "USyBH4BxWXMHNlsuvxh3",
+        "FD_ENDPOINT": "meesun"
+    },
     "WIT_AI_ENDPOINT": "https://api.wit.ai/message?v=20160829&q=",
     "WIT_AI_SERVER_TOKEN": "Bearer BFYEQAC2CHDMSNJY6WI7G7OOA5BLPMD4",
     "SERVER_URL": "https://thebotdesigners.herokuapp.com",
